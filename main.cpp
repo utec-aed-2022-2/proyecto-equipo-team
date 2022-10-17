70bc998f210144b4739a1a71317da0cce9e6c738
diegorivadeneyra <55153225+diegorivadeneyra@users.noreply.github.com>
2022-10-16 22:50:01 -0500

    Menu implementation V1.0

--- main.cpp
+++ main.cpp
@@ -32,10 +32,85 @@ void blockchain_test_1(){
     cryptochain.display();
 }
 
+void Clear()
+{
+    cout << "\x1B[2J\x1B[H";
+}
+
+
+void add_transaction(){
+    Clear();
+    string tex;
+    cout << "\n\n Add_trasnsaction " << endl;
+    cout << "--------------------"<<endl;
+    cout << "Add: ";
+    cin  >> tex;
+
+
+    //Incluir el add
+}
+
+void modification(){
+    Clear();
+    int N;
+    cout << "\n\n Modification " << endl;
+    cout << "--------------------"<<endl;
+    cout << "Numero de bloque: ";
+    cin  >> N;
+
+
+    //Incluir el modification
+}
+
+void Interactive_Menu(){
+  int opcion;
+    bool repetir = true;
+    
+    do {
+        Clear();
+        cout << "\n\n        Menu " << endl;
+        cout << "--------------------"<<endl;
+        cout << "1. Show blocks" << endl;
+        cout << "2. Add transaction" << endl;
+        cout << "3. Modify transaction" << endl;
+        cout << "0. End" << endl;
+        
+        cout << "\nEnter an option: ";
+        cin >> opcion;
+        
+        
+        switch (opcion) {
+            case 1:
+                Clear();
+                int V;
+                blockchain_test_1();
+                do{
+                  cout<<endl<< "Ingrese 0 para salir: ";
+                  cin>> V;
+                  Clear();
+                }while(V!=0);
+                break;
+                
+            case 2:
+                add_transaction();
+                break;
+                
+            case 3:
+                modification();
+                break;
+                
+            case 0:
+            	repetir = false;
+            	break;
+        }        
+    } while (repetir);
+
+}
+
 int main(int argc, char *argv[]){ // test the blockchain, if it is reading correctly the data.
     // sha256_test();
 
-    blockchain_test_1();
+    Interactive_Menu();
 
     return 0;
 }
