19c4a0d4e7f23dba9b949b441f3239e3df3662e9
PedroMO11 <82919499+PedroMO11@users.noreply.github.com>
2022-12-01 03:06:18 -0500

    Create Tuple.h

---
+++ Tuple.h
@@ -0,0 +1,18 @@
+#ifndef CRYPTOCHAIN_TUPLE_H
+#define CRYPTOCHAIN_TUPLE_H
+
+template <typename T>
+class Tuple{
+public:
+    int indice;
+    T value;
+
+    Tuple() = default;
+
+    Tuple(int indice, T value){
+        this->indice = indice;
+        this->value = value;
+    }
+};
+
+#endif //CRYPTOCHAIN_TUPLE_H
