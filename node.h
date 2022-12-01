d401a26904506cd9324787b37af343b8e8c05d9d
PedroMO11 <82919499+PedroMO11@users.noreply.github.com>
2022-12-01 03:09:59 -0500

    Update node.h

--- node.h
+++ node.h
@@ -1,13 +1,15 @@
 #ifndef H_NODE
 #define H_NODE
-
+#include "Tuple.h"
 template <typename T>
 struct Node {
+    int indice;
     T data;
     Node<T>* next;
     Node<T>* prev;
 
     Node(){
+        indice = 0;
         data = 0;
         next = nullptr;
         prev = nullptr;
@@ -16,6 +18,7 @@ struct Node {
 
     explicit Node(T value){
         // TODO         NULL==nullotr
+        indice = 0;
         data = value;
         next = nullptr;
         prev = nullptr;
