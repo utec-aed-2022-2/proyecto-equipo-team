aa74ae268678a4b592951003bd69b7b4c7be5611
PedroMO11 <82919499+PedroMO11@users.noreply.github.com>
2022-12-01 03:02:45 -0500

    Create trie.h

---
+++ trie/trie.h
@@ -0,0 +1,21 @@
+#ifndef CRYPTOCHAIN_TRIE_H
+#define CRYPTOCHAIN_TRIE_H
+
+#include <string>
+
+const unsigned ALPHA_SIZE = 26; // tamanio del alfabeto: letras minusculas
+
+using namespace std;
+
+class Trie
+{
+public:
+    virtual void insert(int indice, string key) = 0;
+    virtual bool search(string key) = 0;
+    virtual void remove(string key) = 0;
+
+    // imprime ordenado
+    virtual string toString(string sep = ",") = 0;
+};
+
+#endif //CRYPTOCHAIN_TRIE_H
