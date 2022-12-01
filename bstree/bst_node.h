5339b372b16c1e6626737aeccdfe67179f7795c7
PedroMO11 <82919499+PedroMO11@users.noreply.github.com>
2022-12-01 02:48:36 -0500

    Update bst_node.h

--- bstree/bst_node.h
+++ bstree/bst_node.h
@@ -3,13 +3,13 @@
 
 template <typename T>
 struct NodeBT {
-    
+    int indice;
     T data;
     NodeBT* left; 
     NodeBT* right;  
     
     NodeBT() : left(nullptr), right(nullptr) {}   
-    NodeBT(T value) : data(value), left(nullptr), right(nullptr) {}   
+    NodeBT(T value) : indice(0), data(value), left(nullptr), right(nullptr) {}
     void KillSelf() {
         if (left != nullptr) left->KillSelf();
         if (right != nullptr) right->KillSelf();
